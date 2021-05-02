124e5fe6f5e5078b823d46eb1eee640072870b3c
Oreda-Z <shtucer31@gmail.com>
2021-05-02 17:22:06 +0300

    Updates

---
+++ precache-manifest.321724da3e40185c84bf2ddb5be3a3de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b9f841a00d014d6a9311dd3dfa8acc",
+    "url": "/super-counter/controlPanel/index.html"
+  },
+  {
+    "revision": "5bf214c9f8fb5f206a61",
+    "url": "/super-counter/controlPanel/static/css/main.dbaa5a13.chunk.css"
+  },
+  {
+    "revision": "27002e2ff8524cf30880",
+    "url": "/super-counter/controlPanel/static/js/2.39e7b6b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/super-counter/controlPanel/static/js/2.39e7b6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf214c9f8fb5f206a61",
+    "url": "/super-counter/controlPanel/static/js/main.55c3daf4.chunk.js"
+  },
+  {
+    "revision": "ab73287e5a393a68e73e",
+    "url": "/super-counter/controlPanel/static/js/runtime-main.8c2265f0.js"
+  }
+]);
